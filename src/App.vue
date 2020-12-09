dd1fd2ca9bfd47e76177bdf546e2c50aaaef4344
Richard Gilbert <richardggilbert@gmail.com>
2020-12-09 19:57:45 +1000

    node google drive stuff

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,7 @@
             @setTag="setTag"
             @shuffleGames="shuffleGames"
         ></Nav>
+        <input type="file" @change="fileChanged">
         <div class="games-area">
             {{ filteredGames.length }} Games
             <div class="games">
@@ -178,6 +179,57 @@ export default {
         })
     },
     methods: {
+        fileChanged(content) {
+            const SCOPES = ['https://www.googleapis.com/auth/drive.metadata.readonly'];
+            const {client_secret, client_id, redirect_uris} = JSON.parse(content).installed;
+            const oAuth2Client = new google.auth.OAuth2(client_id, client_secret, redirect_uris[0]);
+
+            // getAccessToken
+            const authUrl = oAuth2Client.generateAuthUrl({
+                access_type: 'offline',
+                scope: SCOPES,
+            });
+            console.log('Authorize this app by visiting this url:', authUrl);
+
+            // Once token returned
+            // oAuth2Client.getToken(code, (err, token) => {
+            // if (err) return console.error('Error retrieving access token', err);
+            // oAuth2Client.setCredentials(token);
+            // // Store the token to disk for later program executions
+            // fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
+            //     if (err) return console.error(err);
+            //     console.log('Token stored to', TOKEN_PATH);
+            // });
+            // callback(oAuth2Client);
+            // });
+
+
+
+            // google drive request
+            // const drive = google.drive({version: 'v3', auth});
+            // drive.files.list({
+            //     pageSize: 10,
+            //     fields: 'nextPageToken, files(id, name)',
+            // }, (err, res) => {
+            //     if (err) return console.log('The API returned an error: ' + err);
+            //     const files = res.data.files;
+            //     if (files.length) {
+            //     console.log('Files:');
+            //     files.map((file) => {
+            //         console.log(`${file.name} (${file.id})`);
+            //     });
+            //     } else {
+            //     console.log('No files found.');
+            //     }
+            // });
+
+            // Check if we have previously stored a token.
+            // fs.readFile(TOKEN_PATH, (err, token) => {
+            //     if (err) return getAccessToken(oAuth2Client, callback);
+            //     oAuth2Client.setCredentials(JSON.parse(token));
+            //     listFiles(oAuth2Client);
+            // });
+        },
         initGoogleDrive() {
 
             // TODO follow node guide instead
